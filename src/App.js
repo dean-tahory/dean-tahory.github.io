92df67f0e3b7453436d64bbee57a66f717d22efc
Haviv Tahory <havivrayt@mail.tau.ac.il>
2023-04-18 17:47:21 +0300

    Coming Soon Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-
+import myLogo from "./LogoImage.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Coming soon...</p>
+    <div className="fixed bg-zinc-900 h-full w-full">
+      <header className="w-full max-w-2xl px-4 lg:px-0 max-h-fit fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 justify-center items-center text-center">
+        <img src={myLogo} alt="Torli"></img>
+        <p className="text-white font-bold">Coming soon</p>
       </header>
     </div>
   );
